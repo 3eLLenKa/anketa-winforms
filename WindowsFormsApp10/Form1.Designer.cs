3913c277bb2c49e9badfba4eddd5329de0a151bc
Дмитрий Бейкун <alexkomkov39@gmail.com>
2023-10-01 17:40:39 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp10/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(643, 108);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Имя:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(545, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 187);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(545, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(8, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Фамилия:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(110, 213);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(545, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(8, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "E-mail:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(110, 239);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(545, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(8, 236);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Телефон:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 268);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(643, 36);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 16);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Инф-я о пользователях";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(110, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 35);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Редактировать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(371, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 35);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Сохранить в файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 267);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(643, 36);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Сохранить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|XML Files|*.xml";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(667, 315);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Анкета";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Drawing;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
+        private string fName;
+        private string sName;
+        private string email;
+        private string phoneNumber;
+
+        private int temp = 1;
+
+        private List<string> data;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (check())
+            {
+                fName = "Имя: " + textBox1.Text;
+                sName = "Фамилия: " + textBox2.Text;
+                email = "Почта: " + textBox3.Text;
+                phoneNumber = "Телефон: " + textBox4.Text;
+
+                data = new List<string>() { "Пользователь: " + temp, fName, sName ,email, phoneNumber, "\n" };
+
+                for (int i = 0; i < data.Count; i++)
+                {
+                    listBox1.Items.Add(data[i]);
+                }
+
+                foreach (var item in this.Controls.OfType<TextBox>())
+                {
+                    item.Text = string.Empty;
+                }
+                temp++;
+            }
+            else MessageBox.Show("Данные заполнены неправильно!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private bool check()
+        {
+            bool isChecked = true;
+
+            foreach (var item in this.Controls.OfType<TextBox>())
+            {
+                if (item.Text == string.Empty)
+                {
+                    isChecked = false;
+                    break;
+                }
+            }
+
+            if (textBox3.Text.Contains("@mail.ru") == false)
+            {
+                isChecked = false;
+            }
+
+            if (textBox4.Text.Contains("+7") == true & textBox4.Text.Length == 12)
+            {
+                for (int i = 1; i < textBox4.Text.Length; i++)
+                {
+                    if (char.IsNumber(textBox4.Text[i]) == false)
+                    {
+                        isChecked = false;
+                        break;
+                    }
+                }
+            }
+            else isChecked = false;
+
+            return isChecked;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem.ToString().Contains("Пользователь"))
+            {
+                button1.Visible = false;
+                button2.Visible = true;
+                button3.Visible = true;
+            }
+            else
+            {
+                button1.Visible = true;
+                button2.Visible = false;
+                button3.Visible = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = true;
+
+            int count = listBox1.SelectedIndex + 1;
+
+            foreach (var item in this.Controls.OfType<TextBox>().Reverse())
+            {
+                item.Text = listBox1.Items[count].ToString();
+                {
+                    item.Text = item.Text.Replace("Имя: ", "");
+                    item.Text = item.Text.Replace("Фамилия: ", "");
+                    item.Text = item.Text.Replace("Почта: ", "");
+                    item.Text = item.Text.Replace("Телефон: ", "");
+                }
+                count++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int count = listBox1.SelectedIndex;
+            var res = openFileDialog1.ShowDialog();
+            string filePath;
+
+            if (res == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                StreamWriter writer = new StreamWriter(filePath);
+
+                for (int i = 0; i < 5; i++)
+                {
+                    writer.WriteLine(listBox1.Items[count]);
+                    count++;
+                }
+                writer.Close();
+                MessageBox.Show("Успешно!", "Инфа", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (check())
+            {
+                fName = "Имя: " + textBox1.Text;
+                sName = "Фамилия: " + textBox2.Text;
+                email = "Почта: " + textBox3.Text;
+                phoneNumber = "Телефон: " + textBox4.Text;
+
+                data = new List<string>() { fName, sName, email, phoneNumber, "\n" };
+                int count = listBox1.SelectedIndex + 1;
+
+                foreach (var item in this.Controls.OfType<TextBox>())
+                {
+                    item.Text = string.Empty;
+                }
+
+                for (int i = 0; i < data.Count; i++)
+                {
+                    listBox1.Items[count] = data[i];
+                    count++;
+                }
+                button4.Visible = false;
+                button1.Visible = true;
+            }
+            else MessageBox.Show("Данные заполнены неправильно!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
